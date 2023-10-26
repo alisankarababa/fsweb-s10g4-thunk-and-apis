98cd8e28960a9361a74ff7d86c3a63388e0d64c2
Alişan Karababa <alisankarababa@gmail.com>
2023-10-26 16:25:36 +0300

    sayfa açıldığında bir fact alınıyor, favori ekleme butonu, yeni bir api isteği yapacak buton

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
 import Item from "./components/Item";
 import FavItem from "./components/FavItem";
 
+import { fetchAnother, addFav } from "./actions"
+
+import { useSelector, useDispatch } from "react-redux";
+
+
 export default function App() {
   const loading = false;
-  const current = null;
-  const favs = [];
+  const current = useSelector( store => store.catFacts.current);
+  const favs = useSelector( store => store.catFacts.favs);
+  const dispatch = useDispatch();
 
-  function addToFavs() {
-  }
+    useEffect(()=>{
 
+        dispatch(fetchAnother());
+    }, [dispatch])
 
   return (
     <div className="wrapper max-w-xl mx-auto px-4">
@@ -39,12 +46,13 @@ export default function App() {
 
           <div className="flex gap-3 justify-end py-3">
             <button
+                onClick={()=>dispatch(fetchAnother())}
               className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
             >
               Başka bir tane
             </button>
             <button
-              onClick={addToFavs}
+              onClick={() => dispatch(addFav(current))}
               className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white"
             >
               Favorilere ekle
@@ -56,7 +64,7 @@ export default function App() {
           <div className="flex flex-col gap-3">
             {favs.length > 0
               ? favs.map((item) => (
-                <FavItem key={item.key} id={item.key} title={item.activity} />
+                <FavItem key={item.id} id={item.id} fact={item.fact} />
               ))
               : <div className="bg-white p-6 text-center shadow-md">Henüz bir favoriniz yok</div>
             }
